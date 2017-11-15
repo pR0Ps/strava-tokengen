dfba80a6ad676a859d05f862b299bff37c751903
Carey Metcalfe <carey@cmetcalfe.ca>
2017-11-14 22:50:34 -0500

    Include templates in the package

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(name="strava-tokengen",
           "Programming Language :: Python :: 3.6",
       ],
       packages=["strava_tokengen"],
+      package_data={"strava_tokengen": ["templates/*"]},
       install_requires=["CherryPy>=10.2.2,<11.0.0", "Jinja2>=2.9.6,<3.0.0", "stravalib>=0.6.6,<1.0.0"],
       entry_points={'console_scripts': ["strava-tokengen=strava_tokengen:run_server"]}
 )
